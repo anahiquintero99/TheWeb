df6475b26971367ac135c7e666269d64a78d381a
Anahi Quintero <anitaquintero99@gmail.com>
2022-03-16 21:21:10 -0600

    add responsive design

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
-const header = document.querySelector('header');
+let header = document.querySelector('header');
 
 window.addEventListener('scroll', function () {
   header.classList.toggle('sticky', window.scrollY > 0);
 });
+
+function toggle() {
+  let header = document.querySelector('header');
+  header.classList.toggle('active');
+}
